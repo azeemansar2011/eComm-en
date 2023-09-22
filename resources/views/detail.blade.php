6af70c596d974994f9701e928708822c5da4938f
ASUS <ASUS@DESKTOP-CL9NNDE>
2023-09-23 00:38:10 +0530

    add items to cart

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -13,7 +13,12 @@
             <h5>Details : {{$product['description']}}</h5>
             <h5>Category : {{$product['catecory']}}</h5>
             <br><br>
-            <button class="btn btn-primary">Add to Cart</button>
+            <form action="/add_to_cart" method="POST">
+                @csrf
+                <input type="hidden" name="product_id" value="{{$product['id']}}" >
+                <button class="btn btn-primary">Add to Cart</button>
+            </form>
+            <br><br>
             <button class="btn btn-success">Buy Now</button>
         </div>
 
