0abef0d9836a9b9d2d68023e1c3ef4ab16b2cb5c
ASUS <ASUS@DESKTOP-CL9NNDE>
2023-09-23 11:59:02 +0530

    logout route done

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,10 +1,15 @@
 <?php 
 use App\Http\Controllers\ProductController;
-$total=ProductController::cartitem();
+$total=0;
+if(Session::has('user'))
+{
+  $total=ProductController::cartitem();
+}
+
 ?>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg ">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Brand</a>
+    <a class="navbar-brand" href="/">E-Comm</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -25,7 +30,19 @@ $total=ProductController::cartitem();
       </ul>
       <ul class="nav navbar-nav navbar-right">
         <li><a href="#">cart({{$total}})</a></li>
+        @if(Session::has('user'))
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{Session::get('user')['name']}} <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="/logout">Logout</a></li>
+          </ul>
+        </li>
+        @else
+        <li><a href="/login">Login</a></li>
+        @endif
       </ul>
+      
+            
     </div>
   </div>
 </nav>
