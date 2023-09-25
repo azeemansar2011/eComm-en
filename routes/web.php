e258f50071fe84220eee84c4a013de052ae7a705
ASUS <ASUS@DESKTOP-CL9NNDE>
2023-09-25 10:50:43 +0530

    user registration done

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,9 @@ Route::get('/logout', function () {
     return redirect('login');
 });
 
-
+Route::view('/register','register');
 Route::post('/login',[UserController::class,'login']);
+Route::post('/register',[UserController::class,'register']);
 Route::get('/',[ProductController::class,'index']);
 Route::get('detail/{id}',[ProductController::class,'detail']);
 Route::post("add_to_cart",[ProductController::class,'addToCart']);
@@ -32,3 +33,6 @@ Route::get("removecart/{id}",[ProductController::class,'removeCart']);
 Route::get("ordernow",[ProductController::class,'orderNow']);
 Route::post("orderplace",[ProductController::class,'orderPlace']);
 Route::get("myorders",[ProductController::class,'myOrders']);
+
+
+        
